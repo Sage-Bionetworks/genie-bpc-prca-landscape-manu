e4c332cea791a1a310d61378b029cb4d840fbf80
alexpaynter <alexcpaynter@gmail.com>
2023-06-08 22:27:50 +0000

    Created a function from drug plotting.

---
+++ R/plot_drug_prop.R
@@ -0,0 +1,49 @@
+#' @param drug_dat A dataframe with columns "drug" and "n".  Used in the order given.
+plot_drug_prop <- function(drug_dat, cohort_n, pal = NULL, pal_seed = 278, plot_title = NULL) {
+  if (is.null(pal)) {
+    set.seed(pal_seed)
+    pal <- sample(make_sun_pal(nrow(drug_dat)))
+  }
+  
+  drug_dat <- drug_dat %>%
+    select(drug, n) %>%
+    mutate(
+      prop = n/cohort_n,
+      str = glue("n={n} ({formatC(prop*100, digits = 1, format = 'f')}%)")
+    ) %>%
+    mutate(drug = forcats::fct_inorder(drug)) 
+  
+  gg <- ggplot(data = drug_dat,
+               aes(x = prop, y = drug, fill = drug)) +
+    geom_col() + 
+    annotate(
+      geom = 'rect', 
+      xmin = 1, xmax = 1.5, ymin = -2, ymax = 15,
+      color = "gray80", fill = "gray80"
+    ) + 
+    geom_text(size = 2.5, 
+              nudge_x = .05, 
+              hjust = 0, 
+              aes(label = str)) + 
+    labs(title = plot_title) + 
+    scale_fill_manual(
+      values = pal
+    ) + 
+    scale_x_continuous(
+      name = "Proportion of cohort",
+      breaks = c(0,0.5,1),
+      labels = paste(c(0, 50, 100), "%"),
+      expand = expansion(mult = c(0,0.05), add = c(0,0))
+    ) + 
+    scale_y_discrete(limits = rev) + 
+    coord_cartesian(xlim = c(0,1.1), ylim = c(0.9, 10.1)) + 
+    theme_bw() +
+    theme(
+      axis.title.y = element_blank(),
+      legend.position = "none",
+      panel.grid.major.y = element_blank(),
+      panel.grid.minor.y = element_blank()
+    )
+  
+  return(gg)
+}
