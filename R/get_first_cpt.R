2bc10858440b6d07def7889847b134453503240d
alexpaynter <alexcpaynter@gmail.com>
2023-06-21 01:33:54 +0000

    bug fix on get_first_cpt.R

--- R/get_first_cpt.R
+++ R/get_first_cpt.R
@@ -1,7 +1,7 @@
-get_first_cpt <- function(ca_ind_dat) {
+get_first_cpt <- function(ca_ind_dat, cpt_dat) {
   rtn <- ca_ind_dat %>% 
     select(record_id, ca_seq) %>%
-    left_join(., dft_cpt, by = c("record_id", "ca_seq")) %>%
+    left_join(., cpt_dat, by = c("record_id", "ca_seq")) %>%
     arrange(cpt_number) %>%
     group_by(record_id) %>%
     slice(1) %>%
