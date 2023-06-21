0e04444ba81d8d698d8d7f8d2825a78e533fac0d
alexpaynter <alexcpaynter@gmail.com>
2023-06-21 01:40:38 +0000

    Bug fix consequences fixed

--- R/make_event_df.R
+++ R/make_event_df.R
@@ -1,10 +1,11 @@
 
 # Cast_block_dat is the output from make_cast_status_block()
 make_event_df <- function(ca_ind_dat, 
+                          cpt_dat,
                           cast_block_dat) {
   
   # for each index cancer get the first cpt
-  first_panel_dates <- get_first_cpt(ca_ind_dat) %>%
+  first_panel_dates <- get_first_cpt(ca_ind_dat, cpt_dat) %>%
     select(record_id, t_yrs = dx_cpt_rep_yrs)
   
   event_dat <- cast_block_dat %>%
