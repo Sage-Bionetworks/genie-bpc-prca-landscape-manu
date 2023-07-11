cae63ecc8b081594022c3dc8c9fb5c01d236b084
alexpaynter <alexcpaynter@gmail.com>
2023-07-11 11:22:24 -0700

    Added more raw data pulls for genomic info

--- analysis/script/get_raw_data.R
+++ analysis/script/get_raw_data.R
@@ -7,6 +7,16 @@
 
 # The Synapse folder containing the clinical data files.
 synid_clin_data <- "syn50612196"
+synid_cbio_data <- "syn50697637"
+# genomic files to grab (panels are all grabbed based on file name):
+geno_files_included <- c(
+  "data_mutations_extended.txt",
+  "data_CNA.txt",
+  "data_fusions.txt",
+  "data_cna_hg19.seg"
+  # don't see a genomic information file
+)
+  
 
 library(cli)
 library(synapser)
@@ -21,6 +31,8 @@ synLogin()
 # create directories for data and data-raw
 dir.create(here("data"), showWarnings = F)
 dir.create(here("data-raw"), showWarnings = F)
+dir.create(here("data-raw", "genomic"), showWarnings = F)
+
 
 df_clin_children <- synGetChildren(synid_clin_data) %>%
   as.list %>%
@@ -41,14 +53,42 @@ purrr::walk(.x = df_clin_children$id,
 
 
 
+# Get the genomic data from the "cBioPortal_files" directory.
+df_geno_children <- synGetChildren(synid_cbio_data) %>%
+  as.list %>%
+  purrr::map_dfr(.x = .,
+                 .f = as_tibble)
+
+df_geno_children %<>%
+  mutate(
+    is_panel = str_detect(name, "^data_gene_panel_.*\\.txt$"),
+    is_included = name %in% geno_files_included
+  ) %>%
+  filter(is_panel | is_included) 
+
+syn_store_in_dataraw_geno <- function(sid) {
+  synGet(entity = sid, downloadLocation = here("data-raw", "genomic"))
+}
+
+purrr::walk(.x = df_geno_children$id, 
+            .f = syn_store_in_dataraw_geno)
+
+
+
+
+
+
+
+
+
 # genieBPC data pulls
 # In order to create the sunburst plots, we'll leverage the genieBPC to pull the
 #   data in the format they require.
 # To run this, you will need to set up a session with synapse using 
 #   genieBPC::set_synapse_credentials() outside of this script (can't be tracked).
 #   This requires username and password access at the moment.
-library(genieBPC)
-data_list <- pull_data_synapse("Prostate", version = "v1.2-consortium")
-dir.create(here("data-raw", "genieBPC-style"))
-saveRDS(object = data_list, 
-        file = here("data-raw", "genieBPC-style", "data_list.rds"))
+# library(genieBPC)
+# data_list <- pull_data_synapse("Prostate", version = "v1.2-consortium")
+# dir.create(here("data-raw", "genieBPC-style"))
+# saveRDS(object = data_list, 
+#         file = here("data-raw", "genieBPC-style", "data_list.rds"))
