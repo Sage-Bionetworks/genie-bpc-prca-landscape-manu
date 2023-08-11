13156947bf45e5f7536c4a5cb1a87b8059de4002
alexpaynter <alexcpaynter@gmail.com>
2023-08-11 16:40:53 -0700

    Gene panel dataframes created

--- analysis/script/create_gene_panel_dat.R
+++ analysis/script/create_gene_panel_dat.R
@@ -21,4 +21,73 @@ saveRDS(
 
 
 
+gp_all %>% glimpse
+
+
 # Merge in the sample data.
+dft_cpt <- readr::read_rds(
+  here('data', 'clin', 'dft_cpt.rds')
+)
+
+gp_sum <- dft_cpt %>% 
+  select(cpt_genie_sample_id, record_id, cpt_number, 
+         sample_type, cpt_seq_assay_id) %>%
+  group_by(cpt_seq_assay_id) %>%
+  summarize(
+    n_pts = length(unique(record_id)),
+    n_samples = n(),
+    .groups = "drop"
+  )
+
+# Make sure we have a gene panel data file for each one found in the CPT data:
+chk_gp_files <- all(gp_sum$cpt_seq_assay_id %in% unique(gp_all$stable_id))
+if (!chk_gp_files) {
+  cli_abort("At least one gene panel found in the CPT data with no corresponding metadata file")
+}
+
+# get the number of genes in each panel, merge that in to the summary dataframe.
+gp_sum <- gp_all %>%
+  group_by(stable_id) %>%
+  summarize(
+    # n() would also be fine here:
+    n_genes = length(unique(hugo)),
+    .groups = 'drop'
+  ) %>%
+  rename(cpt_seq_assay_id = stable_id) %>%
+  left_join(
+    gp_sum,
+    .,
+    by = "cpt_seq_assay_id"
+  )
+
+gp_sum %<>% 
+  arrange(desc(n_pts)) %>%
+  mutate(
+    cpt_seq_assay_id = forcats::fct_inorder(cpt_seq_assay_id)
+  )
+  
+saveRDS(
+  object = gp_sum,
+  file = here('data', 'genomic', 'gene_panel_sum.rds')
+)
+
+
+
+
+gp_by_gene <- gp_all %>% 
+  group_by(hugo) %>%
+  summarize(
+    n_panels = length(unique(stable_id)),
+    .groups = "drop"
+  ) %>%
+  arrange(desc(n_panels), hugo) %>%
+  mutate(
+    hugo = forcats::fct_inorder(hugo)
+  )
+
+saveRDS(
+  object = gp_by_gene,
+  file = here('data', 'genomic', 'gene_panel_by_gene.rds')
+)
+
+  
