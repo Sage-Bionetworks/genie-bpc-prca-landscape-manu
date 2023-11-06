b1f0c1600f17e6a1919c347c107e5226c0c25838
alexpaynter <alexcpaynter@gmail.com>
2023-11-06 08:57:41 -0800

    Digging more into the MAF tools

--- analysis/explore/co-occur_explore_2.R
+++ analysis/explore/co-occur_explore_2.R
@@ -50,14 +50,43 @@ somaticInteractions(
   plotPadj = T,
   showCounts = T, # show counts and ignore P values.
   countType = "all", # shows co-occur / mut excl.
+  showSum = T,
   pvalue = -Inf,
   colPal = "PuOr", # just for fun.
+  limitColorBreaks = F
 ) 
 
+# A tiny little problem here is the number of tops cahnges the p values dramatically:
 somaticInteractions(
   maf = laml,
-  top = 10,
+  top = 5,
+  plotPadj = T
+)
+somaticInteractions(
+  maf = laml,
+  top = 5,
   plotPadj = T
 )
 
-  
+# Two things we haven't touched on yet:
+# - Getting the list of "top" genes, done by getGeneSummary().
+# - Making sure the P value adjustment and end-to-end steps line up with what
+#   maftools is doing.
+
+getGeneSummary(laml) %>% head # which just pulls
+laml@gene.summary %>% head
+# So the numbers in brackets are the number of altered samples.
+
+# The counts of altered samples look unfiltered - the "total" just counts
+#   total number of alterations without accounting for 2+ in specific samples.
+laml@data %>% 
+  as_tibble(.) %>%
+  dplyr::count(Tumor_Sample_Barcode, Hugo_Symbol) %>%
+  dplyr::count(Hugo_Symbol, sort = T, name = "AlteredSamples")
+laml@gene.summary %>% head(., 10) %>% select(Hugo_Symbol, AlteredSamples)
+
+# Last thing to do: Make sure that the P value adjustment I get from top genes
+#   gives me identical results to what they had.  Probably time to just make
+#   a function I think.
+
+
